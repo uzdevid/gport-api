9e88c3bf64bb00602c7243b8aba444e76abd24cc
uzdevid <uzdevid@gmail.com>
2024-05-29 17:30:54 +0500

    Доработки

--- socket/Message/SharingResponse.php
+++ socket/Message/SharingResponse.php
@@ -6,7 +6,6 @@ use uzdevid\property\loader\Entity;
 
 class SharingResponse extends Entity {
     public int $id;
-    public string $key;
     public string $remoteAddress;
     public string $localAddress;
     public array $proxies = [];
@@ -16,12 +15,12 @@ class SharingResponse extends Entity {
      */
     protected function properties(): array {
         return [
-            'remoteAddress' => [fn(string $remote_address) => sprintf("https://%s", $remote_address), 'remote_address'],
-            'localAddress' => [fn(string $remote_address) => sprintf("https://%s", $remote_address), 'local_address'],
+            'remoteAddress' => [fn(string $remote_address) => sprintf("https://%s", $remote_address), 'remote'],
+            'localAddress' => [fn(string $remote_address) => sprintf("https://%s", $remote_address), 'local'],
             'proxies' => [fn(string $remote_address, string $local_address) => [
                 sprintf("https://%s", $remote_address) => $local_address,
                 sprintf("http://%s", $remote_address) => $local_address
-            ], 'remote_address', 'local_address']
+            ], 'remote', 'local']
         ];
     }
 }
